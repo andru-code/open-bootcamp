3e302fb4ba01b7120b769be03e2901b042bdcc28
Andrés Felipe Rios <andrucode@gmail.com>
2022-09-27 19:04:11 -0500

    Tercer ejercicio

---
+++ ob-exercise3/src/main/java/com/example/obexercise3/ObExercise3Application.java
@@ -0,0 +1,13 @@
+package com.example.obexercise3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObExercise3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObExercise3Application.class, args);
+	}
+
+}
